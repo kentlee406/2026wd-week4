5f59036f44c8a2af6d711882d083c0d4d5352bc2
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2026-07-17 02:12:04 +0800

    version 2

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 import "./assets/scss/all.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+const accordionTriggers = document.querySelectorAll(".accordion-trigger");
+
+accordionTriggers.forEach((trigger) => {
+	trigger.addEventListener("click", () => {
+		const panelId = trigger.getAttribute("aria-controls");
+
+		if (!panelId) {
+			return;
+		}
+
+		const panel = document.getElementById(panelId);
+
+		if (!panel) {
+			return;
+		}
+
+		const isExpanded = trigger.getAttribute("aria-expanded") === "true";
+
+		trigger.setAttribute("aria-expanded", String(!isExpanded));
+		panel.hidden = isExpanded;
+	});
+});
